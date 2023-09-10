0d1105b365f22ef64ec3509e44c9b8f6bc4d2853
Patrick Jessen <patrick.jessen@icloud.com>
2023-09-10 12:47:41 +0200

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </div>
           <Analytics />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
